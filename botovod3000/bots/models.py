605881d88ad664a99f54b31db7e032acdb21e97a
sysadmin <d1vinel3af>
2025-07-17 15:55:05 +0300

    Добавил получаение карточек с информацией о ботах из бд

--- botovod3000/bots/models.py
+++ botovod3000/bots/models.py
@@ -6,3 +6,6 @@ class BotModel(models.Model):
     pair = models.CharField(max_length=10)
     status = models.CharField(max_length=5)
     profit = models.FloatField()
+
+    def __str__(self):
+        return f"{self.title} | {self.pair} | {self.status}"
--- botovod3000/control_panel/views.py
+++ botovod3000/control_panel/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from bots.models import BotModel
 
 def control_panel(request):
-    return render(request, "control_panel/index.html")
+    list_bots = BotModel.objects.all()
+
+    return render(request, "control_panel/index.html",
+                  context={
+                      "bots": list_bots
+                  })
